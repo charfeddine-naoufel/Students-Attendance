d90a7afd709f892dcab93e2c89d579a7067082f9
charfeddine-naoufel <62994930+charfeddine-naoufel@users.noreply.github.com>
2023-03-27 00:10:01 +0200

    refactoring prof dashboard

--- resources/views/prof/layouts/master.blade.php
+++ resources/views/prof/layouts/master.blade.php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="42W8IaY1w7BErjQOEH7syA4lyNyWBkWkLdgnmhgB">
-    <title>@yield('title')</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,400i,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@500&family=Cairo:wght@400;500&family=Noto+Naskh+Arabic:wght@600&display=swap" rel="stylesheet">
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@500&display=swap" rel="stylesheet">  -->
-    <link id="gull-theme" rel="stylesheet" href="{{asset('assets/styles/css/themes/lite-purple.min.css')}}">
-    <link id="gull-theme" rel="stylesheet" href="{{asset('assets/fonts/fontawesome-free-5.12.1-web/css/all.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/styles/vendor/perfect-scrollbar.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/styles/vendor/sweetalert2.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/styles/css/select2.min.css')}}">
-    <!-- <link id="gull-theme" rel="stylesheet" href="http://gull-html-laravel.ui-lib.com/assets/styles/css/themes/lite-purple.min.css">
-        <link rel="stylesheet" href="http://gull-html-laravel.ui-lib.com/assets/styles/vendor/perfect-scrollbar.css"> -->
-    @yield('customstyle')
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="qM3C6SQ9HgjfYwXu6u6HkXL3jS69eJTjaidMTZmA">
+        <title>@yield('title')</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,400i,600,700,800,900" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@500&family=Cairo:wght@400;500&family=Noto+Naskh+Arabic:wght@600&display=swap" rel="stylesheet">
+                    
+        <link id="gull-theme" rel="stylesheet" href="{{asset('assets/styles/css/themes/lite-purple.min.css')}}">
+        <link id="gull-theme" rel="stylesheet" href="{{asset('assets/fonts/fontawesome-free-5.12.1-web/css/all.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/styles/vendor/perfect-scrollbar.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/styles/vendor/sweetalert2.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/styles/css/select2.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/styles/vendor/metisMenu.min.css')}}">
+        
+
+        @yield('customstyle')
+                
+            </head>
 
 
-<body class="text-left">
+    <body class="text-left">
+        
+        <!-- Pre Loader Strat  -->
+        <div class='loadscreen' id="preloader">
 
-    <!-- Pre Loader Strat  -->
-    <!-- <div class='loadscreen' id="preloader">
-
-        <div class="loader spinner-bubble spinner-bubble-primary">
+            <div class="loader spinner-bubble spinner-bubble-primary">
 
 
+            </div>
         </div>
-    </div> -->
-    <!-- Pre Loader end  -->
+        <!-- Pre Loader end  -->
 
 
 
@@ -41,98 +42,155 @@
 
 
 
-    <!-- ============ Compact Layout start ============= -->
+        <!-- ============ Compact Layout start ============= -->
+                <div class="app-admin-wrap layout-sidebar-vertical sidebar-full">
+            <!-- start sidebar -->
+<div class="sidebar-panel">
+    <div class="gull-brand pr-3 text-center mt-4 mb-2 d-flex justify-content-center align-items-center">
+        <img class="pl-3" src="{{asset('assets/images/logo.png')}}" alt="">
+        <span class=" item-name text-20 text-primary font-weight-700">LS Med Ali</span>
+        <!-- <div class="sidebar-compact-switch ml-auto"><span></span></div> -->
 
-    <div class="app-admin-wrap layout-sidebar-compact sidebar-dark-purple sidenav-open clearfix">
-        <div class="side-content-wrap">
+    </div>
+    <!-- user -->
+    <div class="scroll-nav" data-perfect-scrollbar data-suppress-scroll-x="true">
 
-            @include('prof.layouts.menu')
+        <!-- user close -->
+        <!-- side-nav start -->
+        <div class="side-nav">
 
-            <div class="sidebar-overlay"></div>
-        </div>
-        <!--=============== Left side End ================-->
-        <!-- ============ end of left sidebar ============= -->
+            <div class="main-menu">
 
-
-        <!-- ============ Body content start ============= -->
-        <div class="main-content-wrap d-flex flex-column">
-            <div class="main-header">
-                <div class="logo">
-                    <img src="{{asset('assets/images/logo.png')}}" alt="">
-                </div>
-
-                <div class="menu-toggle">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-
-                <div class="d-flex align-items-center">
-
-                    <div class="search-bar">
-                        <input type="text" placeholder="Search">
-                        <i class="search-icon text-muted i-Magnifi-Glass1"></i>
-                    </div>
-                </div>
-
-                <div style="margin: auto"></div>
-
-                <div class="header-part-right">
-                    <!-- Full screen toggle -->
-                    <i class="i-Full-Screen header-icon d-none d-sm-inline-block" data-fullscreen></i>
-                    <!-- Grid menu Dropdown -->
-
-                    <!-- Notificaiton -->
-                    <div class="dropdown">
-                        <div class="badge-top-container" role="button" id="dropdownNotification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="badge badge-primary">3</span>
-                            <i class="i-Bell text-muted header-icon"></i>
-                        </div>
-                        <!-- Notification dropdown -->
-                        <div class="dropdown-menu dropdown-menu-right notification-dropdown rtl-ps-none" aria-labelledby="dropdownNotification" data-perfect-scrollbar data-suppress-scroll-x="true">
-                            <div class="dropdown-item d-flex">
-                                <div class="notification-icon">
-                                    <i class="i-Speach-Bubble-6 text-primary mr-1"></i>
-                                </div>
-
-                            </div>
-
-
-
-                        </div>
-                    </div>
-                    <!-- Notificaiton End -->
-
-                    <!-- User avatar dropdown -->
-                    <div class="dropdown">
-                <div class="user col align-self-end">
-                    <img src="{{asset('assets/images/logon.png')}}" id="userDropdown" alt="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <div class="dropdown-header">
-                            <i class="i-Lock-User mr-1"></i> {{Auth::user()->name}}
-                        </div>
-                        <a class="dropdown-item">Profile</a>
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
+                <!-- <ul>
+                            <li>
+                                <a href="*" class="active-color">
+                                    <span class="item-name ">Dashboard</span>
+                                    <span class="spacer"></span>
+                                    <span class="item-badge badge badge-warning">100+</span>
+                                </a>
+                            </li>
+                            
+                        </ul> -->
+                <ul class="metismenu" id="menu">
+                    <!-- <p class="main-menu-title text-muted ml-3 font-weight-700 text-13 mt-4 mb-2">Apps</p> -->
+                    <li class="Ul_li--hover">
+                        <a class="has-arrow" href="{{route('prof.home')}}">
+                            <i class="i-Bar-Chart text-20 mr-2 text-muted"></i>
+                            <span class="item-name  text-muted">Dashboard</span>
                         </a>
+                        
+                    </li>
+                    <li class="Ul_li--hover">
+                        <a class="has-arrow" href="#">
+                            <i class="i-Library text-20 mr-2 text-muted"></i>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
+                            <span class="item-name  text-muted">Classes</span>
+                        </a>
+                        <ul class="mm-collapse">
+                            <li class="item-name">
+                                <a href="{{route('prof.mesclasses')}}">
+                                    <i class="nav-icon i-Bell1"></i>
+                                    <span class="item-name">Mes classes</span>
+                                </a>
+                            </li>
+                            <li class="item-name">
+                            <li class="nav-item">
+                                <a href="#">
+                                    <i class="nav-icon i-Bell1"></i>
+                                    <span class="item-name">Mes élèves</span>
+                                </a>
+                            </li>
+                    </li>
+                   
+                    
+                </ul>
+                </li>
+                
+               
+
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- side-nav-close -->
+</div>
+<!-- end sidebar -->
+<div class="switch-overlay"></div>            <div class="main-content-wrap  mobile-menu-content bg-off-white m-0">
+                <!-- header start -->
+<header class=" main-header bg-white d-flex justify-content-between p-2">
+    <div class="header-toggle">
+        <div class="menu-toggle mobile-menu-icon">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+        <i class="i-Add-UserStar mr-3 text-20 cursor-pointer" data-toggle="tooltip" data-placement="top" title=""
+            data-original-title="Todo"></i>
+        <i class="i-Speach-Bubble-3 mr-3 text-20 cursor-pointer" data-toggle="tooltip" data-placement="top" title=""
+            data-original-title="Chat"></i>
+        <i class="i-Email mr-3 text-20 mobile-hide cursor-pointer" data-toggle="tooltip" data-placement="top" title=""
+            data-original-title="Inbox"></i>
+        <i class="i-Calendar-4 mr-3 mobile-hide text-20 cursor-pointer" data-toggle="tooltip" data-placement="top"
+            title="" data-original-title="Calendar"></i>
+        <i class="i-Checkout-Basket mobile-hide mr-3 text-20 cursor-pointer" data-toggle="tooltip" data-placement="top"
+            title="" data-original-title="Calendar"></i>
+
+    </div>
+    <div class="header-part-right">
+        <!-- Full screen toggle -->
+        <i class="i-Full-Screen header-icon d-none d-sm-inline-block" data-fullscreen></i>
+        <!-- Grid menu Dropdown -->
+
+        <!-- Notificaiton -->
+        <div class="dropdown">
+            <div class="badge-top-container" role="button" id="dropdownNotification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="badge badge-primary">3</span>
+                <i class="i-Bell text-muted header-icon"></i>
+            </div>
+            <!-- Notification dropdown -->
+            <div class="dropdown-menu dropdown-menu-right notification-dropdown rtl-ps-none" aria-labelledby="dropdownNotification" data-perfect-scrollbar data-suppress-scroll-x="true">
+                <div class="dropdown-item d-flex">
+                    <div class="notification-icon">
+                        <i class="i-Speach-Bubble-6 text-primary mr-1"></i>
                     </div>
+
+                </div>
+
+
+
+            </div>
+        </div>
+        <!-- Notificaiton End -->
+
+        <!-- User avatar dropdown -->
+        <div class="dropdown">
+            <div class="user col align-self-end">
+                <img src="{{asset('assets/images/logon.png')}}" id="userDropdown" alt="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                    <div class="dropdown-header">
+                        <i class="i-Lock-User mr-1"></i> {{Auth::user()->name}}
+                    </div>
+                    <a class="dropdown-item">Profile</a>
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </div>
             </div>
-                </div>
+        </div>
+    </div>
+</header>
+<!-- header close -->
+@yield('content') 
 
-            </div>
-            <!-- header top menu end -->
-
-            <!-- ============ end of header menu ============= -->
-            @yield('content')
-
-            <!-- Footer Start -->
+              <!-- Footer Start -->
             <div class="flex-grow-1"></div>
             <div class="app-footer">
 
@@ -150,18 +208,16 @@
                 </div>
             </div>
             <!-- fotter end -->
+            </div>
+
+            <div class="sidebar-overlay open"></div>
         </div>
-        <!-- ============ Body content End ============= -->
-    </div>
-    <!--=============== End app-admin-wrap ================-->
 
 
 
 
+        <!-- ============ Vetical SIdebar Layout End ============= -->
 
-
-
-    <!-- ============ Compact Layout End ============= -->
 
 
 
@@ -173,64 +229,41 @@
 
 
 
-    <!-- ============ Horizontal Layout start ============= -->
+        <!-- ============ Large SIdebar Layout start ============= -->
 
 
 
+        
+        <script src="{{asset('assets/js/common-bundle-script.js')}}"></script>
+        
+             <script src="{{asset('assets/js/vendor/echarts.scripts.js')}}"></script>
+     <!-- ph<script src="{{asset('assets/js/es5/echart.options.js')}}"></script> -->
+     <script src="{{asset('assets/js/es5/dashboard.v1.script.js')}}"></script>
 
 
+        
+        
+        <script src="{{asset('assets/js/script.js')}}"></script>
 
 
+        
 
 
-
-    </div>
-    </div>
-    </div>
-    <!-- ============ End Customizer ============= -->
-
-
-    <script src="{{asset('assets/js/common-bundle-script.js')}}"></script>
-
-    <script src="{{asset('assets/js/vendor/jquery-3.3.1.min.js')}}"></script>
-    <script src="{{asset('assets/js/vendor/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('assets/js/vendor/perfect-scrollbar.min.js')}}"></script>
-    <script src="{{asset('assets/js/vendor/echarts.min.js')}}"></script>
-
-    <script src="{{asset('assets/js/es5/echart.options.min.js')}}"></script>
-    <script src="{{asset('assets/js/es5/dashboard.v1.script.min.js')}}"></script>
-
-    <script src="{{asset('assets/js/es5/script.min.js')}}"></script>
-    <script src="{{asset('assets/js/es5/sidebar.large.script.min.js')}}"></script>
-    <script src="{{asset('assets/js/vendor/sweetalert2.min.js')}}"></script>
-
-    <!-- <script src="{{asset('assets/js/sweetalert.script.js')}}"></script>  -->
-
-    <script src="{{asset('assets/js/modal.script.js')}}"></script>
-    <script src="{{asset('assets/js/customizer.script.js')}}"></script>
-    <script src="{{asset('assets/js/script.js')}}"></script>
-    <script src="{{asset('assets/fonts/fontawesome-free-5.12.1-web/js/all.min.js')}}"></script>
+        <script src="{{asset('assets/js/tooltip.script.js')}}"></script>
+        <script src="{{asset('assets/js/es5/script_2.js')}}"></script>
+        <script src="{{asset('assets/js/vendor/feather.min.js')}}"></script>
+        <script src="{{asset('assets/js/vendor/metisMenu.min.js')}}"></script>
+        <script src="{{asset('assets/js/layout-sidebar-vertical.js')}}"></script>
 
 
-    <!-- <script src="http://gull-html-laravel.ui-lib.com/assets/js/common-bundle-script.js"></script> -->
+        
 
 
+        <script src="{{asset('assets/js/customizer.script.js')}}"></script>
+        <script type="text/javascript" src="addon/fonction/fonction.js" ></script>
+        
+        @yield('scripts')
 
-
-    <!-- <script src="http://gull-html-laravel.ui-lib.com/assets/js/script.js"></script> -->
-
-
-    <!-- <script src="http://gull-html-laravel.ui-lib.com/assets/js/sidebar.compact.script.js"></script> -->
-
-
-
-
-
-    <!-- <script src="http://gull-html-laravel.ui-lib.com/assets/js/customizer.script.js"></script> -->
-    @yield('scripts')
-
-
-
-</body>
+            </body>
 
 </html>
